91ce8664a9b105fbc5f507210e5b48b965304a80
rhangai <hangai.renan@gmail.com>
2020-03-10 21:41:37 -0300

    chore: Updated nuxt

--- nuxt/index.js
+++ nuxt/index.js
@@ -4,6 +4,7 @@ export default function() {
 	this.addPlugin({
 		src: path.resolve(__dirname, "plugin.js"),
 		options: {
+			AuthContainer: path.resolve(__dirname, "../lib/"),
 			scheme: {
 				module: path.resolve(__dirname, "../lib/scheme/SchemeOAuth"),
 				moduleImport: "SchemeOAuth",

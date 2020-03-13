42434ce772ee4719c32b751c62710dfeaca3a60d
rhangai <hangai.renan@gmail.com>
2020-03-13 12:29:54 -0300

    chore: Added better typings

--- nuxt/index.d.ts
+++ nuxt/index.d.ts
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import { NuxtAppOptions } from "@nuxt/types/app";
 import { IAuthContainer } from "../types/AuthContainer";
 
 /**
@@ -6,6 +7,12 @@ import { IAuthContainer } from "../types/AuthContainer";
  */
 declare module "vue/types/vue" {
 	interface Vue {
+		$auth: IAuthContainer;
+	}
+}
+
+declare module "@nuxt/types/app" {
+	interface NuxtAppOptions {
 		$auth: IAuthContainer;
 	}
 }
